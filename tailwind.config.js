bd7e53387620d78b2bce8b4c514aa5ba59e37849
AElfadel <125569860+AElfadel@users.noreply.github.com>
2023-05-09 20:00:12 +0300

    Started my own no help chatgpt clone

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      'gptbgd' : '#202123',
+      'gptbgl' : '#343542',
+      'offwhite' : '#ececf2',
+      'gptgreen' : '#13b78a',
+      'gptlight' : '#ececf1',
+
+    }
     },
   },
   plugins: [],
