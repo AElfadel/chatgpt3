6107a3c6ee7d6b95cff20f0c527a06c9e567fc96
AElfadel <125569860+AElfadel@users.noreply.github.com>
2023-05-16 18:30:34 +0300

    Revert "Revert "chat row functioning""
    This reverts commit be84abfb487418c8f957b298ac4411671136a4e5.

--- components/ChatRow.tsx
+++ components/ChatRow.tsx
@@ -1,23 +1,58 @@
+import { db } from "@/firebase";
 import { ChatBubbleLeftIcon } from "@heroicons/react/24/outline";
 import { TrashIcon } from "@heroicons/react/24/outline";
+import { collection, deleteDoc, doc } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useCollection } from "react-firebase-hooks/firestore";
 
 type Props = {
   id: String;
 };
 
-function ChatRow() {
+function ChatRow({ id }: Props) {
+  const { data: session } = useSession();
+  const router = useRouter();
+  const pathname = usePathname();
+  const [active, setActive] = useState(false);
+
+  //I havent used react firebase hooks to get the messages content and then display it as the chat title in the chat row because I havent added messages yet. But this is not enought. Lets rewrtie this page again to make sure we got it XP
+
+  const deleteChat = async () => {
+    await deleteDoc(doc(db, "users", session?.user?.email!, "chats", id));
+    router.push("/");
+  };
+
+  useEffect(() => {
+    if (!pathname) return;
+
+    setActive(pathname.includes(id));
+  }, [pathname]);
+
   return (
-    <div>
-      <p className="text-darkwhite text-sm font-black text-right pr-4 pt-3">
-        محادثة جديدة
-      </p>
-      <div>
-        <div className="flex text-small py-2 px-3 items-center gap-3 relative rounded-md hover:bg-[#2A2B32] cursor-pointer pr-4 ">
+    <div className="text-white">
+      <Link
+        href={`chat/${id}`}
+        className={`flex py-2.5 px-3 items-center gap-3 relative rounded-md cursor-pointer hover:bg-gptbgl/50 ${
+          active && "bg-gptbgl hover:bg-gptbgl"
+        }`}
+      >
+        <div className="flex flex-1">
           <ChatBubbleLeftIcon className="w-4 h-4" />
-          <p className="text-white">بعض الكتابة ها هنا</p>
-          <TrashIcon className="w-4 h-4 text-darkwhite hover:text-white" />
+          <p className="px-2">محادثة جديدة</p>
+          <div className="absolute inset-y-0 left-0 w-8 z-10 bg-gradient-to-r from-gptbgd group-hover:from-[#2A2B32]"></div>
         </div>
-      </div>
+
+        {active && (
+          <TrashIcon
+            onClick={deleteChat}
+            className="w-4 h-4 z-20 text-white hover:text-white"
+          />
+        )}
+      </Link>
     </div>
   );
 }
